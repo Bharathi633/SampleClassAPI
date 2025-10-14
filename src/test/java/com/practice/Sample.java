31291bf00bd2192a7a7d248830aed4221e576f8e
Bharathi <bharathismart75@gmail.com>
2025-10-14 21:17:32 +0530

    message

---
+++ src/test/java/com/practice/Sample.java
@@ -0,0 +1,21 @@
+package com.practice;
+
+public class Sample {
+	
+	private void test1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void test2() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void test3() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
