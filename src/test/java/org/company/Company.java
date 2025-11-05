18c71b02b0c941dae83dbbf4ad2a5cf081a4bae2
Bharathi <bharathismart75@gmail.com>
2025-11-05 21:18:31 +0530

    added methods and class

---
+++ src/test/java/org/company/Company.java
@@ -0,0 +1,24 @@
+package org.company;
+
+public class Company {
+	
+	private void method1() {
+
+		System.out.println("method1");
+	}
+	
+	private void method2() {
+
+		System.out.println("Method2");
+	}
+	
+	private void method3() {
+
+		System.out.println("Method3");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
